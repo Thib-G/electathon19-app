65d1cebfc45cdad26a47693c3cb5e71e078e071a
Thib <thibaut.g@protonmail.com>
2019-05-25 15:16:45 +0200

    Add link to source in About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-card>
+      <p>Using
+        <a href="https://github.com/jbelien/elections-be-api" target="_blank">@jbelien's API</a>
+        from OpenKnowledge Belgium's
+        <a href="https://elections.openknowledge.be/" target="_blank">Electathon 2019</a>
+        to retrieve election results.</p>
+      <p>Source code is available on
+        <a href="https://github.com/Thib-G/electathon19-app/" target="_blank">Github</a>.</p>
+    </b-card>
   </div>
 </template>
