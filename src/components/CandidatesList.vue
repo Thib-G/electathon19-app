1e2183dc31192f553bc98e58512548c2edbc5482
Thib <thibaut.g@protonmail.com>
2019-05-25 17:05:44 +0200

    Select entity

--- src/components/CandidatesList.vue
+++ src/components/CandidatesList.vue
@@ -11,18 +11,21 @@
           <b-card
             v-for="list in entity.values"
             :key="list.key"
-            :header="list.key"
+            header-tag="header"
             class="mt-2">
-            <ul>
-              <li v-for="type in list.values" :key="type.key">
-                {{ type.key }}
+            <div slot="header">
+              <span class="dot mr-1" :style="{ 'background-color': list.color }"></span>
+              {{ list.key }}
+            </div>
+            <b-card-group deck>
+              <b-card v-for="type in list.values" :key="type.key" :header="type.key">
                 <ul>
                   <li v-for="c in type.values" :key="c.key">
                     {{ c.value.nr }} {{ c.value.name }}
                   </li>
                 </ul>
-              </li>
-            </ul>
+              </b-card>
+            </b-card-group>
           </b-card>
         </b-card>
       </b-col>
@@ -36,37 +39,48 @@ import d3 from '@/assets/d3';
 export default {
   props: {
     candidates: {
-      type: Object,
+      type: Array,
       required: true,
     },
   },
+  data() {
+    return {
+      types: {
+        E: 'Effective',
+        S: 'Deputy (suppléant)',
+      },
+    };
+  },
   computed: {
-    candidatesArray() {
-      return Object.keys(this.candidates)
-        .map(k => ({
-          key: k,
-          value: this.candidates[k],
-        }))
-        .sort((a, b) => a.value.list.nr - b.value.list.nr)
-        .sort((a, b) => a.value.list.entity.id - b.value.list.entity.id);
-    },
     candidatesByEntityListType() {
       return d3.nest()
         .key(d => d.value.list.entity.id)
-        .entries(this.candidatesArray)
+        .entries(this.candidates)
         .map(entity => ({
           key: entity.key,
           name: entity.values[0].value.list.entity.name_en,
           values: d3.nest()
             .key(d => `${d.value.list.nr} ${d.value.list.name}`)
-            .entries(entity.values).map(type => ({
-              key: type.key,
+            .entries(entity.values)
+            .map(list => ({
+              key: list.key,
+              color: `#${list.values[0].value.list.group.color}`,
               values: d3.nest()
-                .key(d => d.value.type)
-                .entries(type.values),
+                .key(d => this.types[d.value.type])
+                .entries(list.values),
             })),
         }));
     },
   },
 };
 </script>
+
+<style>
+.dot {
+  height: 12px;
+  width: 12px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
--- src/views/Candidates.vue
+++ src/views/Candidates.vue
@@ -15,14 +15,32 @@
               v-model="selected"
               :options="options"
               required
-              placeholder="Chose type"
+              placeholder="Select type"
+            ></b-form-select>
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group
+            id="input-group-2"
+            label="Select entity:"
+            label-for="input-2"
+          >
+            <b-form-select
+              id="input-1"
+              v-model="selectedEntity"
+              :options="entitiesOptions"
+              required
+              placeholder="Select entity"
+              :disabled="entitiesOptions.length === 0"
             ></b-form-select>
           </b-form-group>
         </b-col>
       </b-row>
       <b-row class="mt-2">
         <b-col>
-          <CandidatesList v-if="candidates" :candidates="candidates" />
+          <CandidatesList
+            v-if="filteredCandidates.length > 0"
+            :candidates="filteredCandidates" />
         </b-col>
       </b-row>
     </b-card>
@@ -30,6 +48,7 @@
 </template>
 
 <script>
+import d3 from '@/assets/d3';
 import ElectathonService from '@/services/electathon-service';
 
 import CandidatesList from '@/components/CandidatesList.vue';
@@ -48,14 +67,44 @@ export default {
     return {
       electathonService: ElectathonService,
       selected: null,
+      selectedEntity: null,
       options,
-      candidates: null,
+      candidatesObj: null,
     };
+  },
+  computed: {
+    candidates() {
+      if (!this.candidatesObj) {
+        return [];
+      }
+      return Object.keys(this.candidatesObj)
+        .map(k => ({
+          key: k,
+          value: this.candidatesObj[k],
+        }))
+        .sort((a, b) => a.value.list.nr - b.value.list.nr)
+        .sort((a, b) => a.value.list.entity.id - b.value.list.entity.id);
+    },
+    filteredCandidates() {
+      return this.candidates.filter(d => d.value.list.entity.id === this.selectedEntity);
+    },
+    entities() {
+      return d3.nest()
+        .key(d => d.value.list.entity.id)
+        .rollup(leaves => leaves[0].value.list.entity.name_en)
+        .entries(this.candidates);
+    },
+    entitiesOptions() {
+      return this.entities.map(d => ({
+        value: +d.key,
+        text: d.value,
+      }));
+    },
   },
   methods: {
     getCandidates() {
       this.electathonService.getCandidates(this.selected).then((data) => {
-        this.candidates = data;
+        this.candidatesObj = data;
       });
     },
   },
