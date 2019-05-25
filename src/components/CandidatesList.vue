65c00fd44784f50e960ee3588bb01b8384542f02
Thib <thibaut.g@protonmail.com>
2019-05-25 16:03:58 +0200

    Improve nesting

--- src/components/CandidatesList.vue
+++ src/components/CandidatesList.vue
@@ -4,15 +4,26 @@
     <b-row class="mt-2">
       <b-col>
         <b-card
-          v-for="list in candidatesByList"
-          :key="list.key"
-          :header="list.key"
+          v-for="entity in candidatesByEntityListType"
+          :key="entity.key"
+          :header="entity.name"
           class="mt-2">
-          <ul>
-            <li v-for="c in list.values" :key="c.key">
-              {{ c.value.nr }} {{ c.value.name }}
-            </li>
-          </ul>
+          <b-card
+            v-for="list in entity.values"
+            :key="list.key"
+            :header="list.key"
+            class="mt-2">
+            <ul>
+              <li v-for="type in list.values" :key="type.key">
+                {{ type.key }}
+                <ul>
+                  <li v-for="c in type.values" :key="c.key">
+                    {{ c.value.nr }} {{ c.value.name }}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </b-card>
         </b-card>
       </b-col>
     </b-row>
@@ -36,12 +47,25 @@ export default {
           key: k,
           value: this.candidates[k],
         }))
-        .sort((a, b) => a.value.list.nr - b.value.list.nr);
+        .sort((a, b) => a.value.list.nr - b.value.list.nr)
+        .sort((a, b) => a.value.list.entity.id - b.value.list.entity.id);
     },
-    candidatesByList() {
+    candidatesByEntityListType() {
       return d3.nest()
-        .key(d => `${d.value.list.nr} ${d.value.list.name}`)
-        .entries(this.candidatesArray);
+        .key(d => d.value.list.entity.id)
+        .entries(this.candidatesArray)
+        .map(entity => ({
+          key: entity.key,
+          name: entity.values[0].value.list.entity.name_en,
+          values: d3.nest()
+            .key(d => `${d.value.list.nr} ${d.value.list.name}`)
+            .entries(entity.values).map(type => ({
+              key: type.key,
+              values: d3.nest()
+                .key(d => d.value.type)
+                .entries(type.values),
+            })),
+        }));
     },
   },
 };
