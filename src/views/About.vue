0ebc4f1111f095f979a1ec35178a20a084c411fd
Thib <thibaut.g@protonmail.com>
2019-05-26 16:50:06 +0200

    Add attribution

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <b-card>
+      <h3>Acknowledgments</h3>
       <p>Using
         <a href="https://github.com/jbelien/elections-be-api" target="_blank">@jbelien's API</a>
         from OpenKnowledge Belgium's
@@ -9,6 +10,10 @@
         to retrieve election results.</p>
       <p>Source code is available on
         <a href="https://github.com/Thib-G/electathon19-app/" target="_blank">Github</a>.</p>
+      <h3>Contact</h3>
+      <p>Author: Thibaut Goelff<br />
+        <small><a href="https://twitter.com/tgoelff" target="_blank">Twitter</a>
+        | <a href="https://www.linkedin.com/in/thibautgoelff/" target="_blank">LinkedIn</a></small></p>
     </b-card>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>Welcome to Electathon 2019</h1>
     <b-card>
-      <h2>Playing with elections 2019 open data</h2>
+      <h2>Playing with Belgian elections 2019 open data</h2>
       <ul>
         <li v-for="r in $router.options.routes" :key="r.name">
           <router-link :to="r.path">{{ r.meta.longName }}</router-link>
@@ -17,7 +17,6 @@
       <h3>Source code</h3>
       <p>Source code is available on
         <a href="https://github.com/Thib-G/electathon19-app" target="_blank">Github</a>.</p>
-      <p>Contact: Thibaut Goelff <a href="https://twitter.com/tgoelff" target="_blank">@tgoelff</a>.</p>
     </b-card>
   </div>
 </template>
