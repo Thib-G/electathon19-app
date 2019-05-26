2cee3da5278881f1779eddabdaefb8a0f056d429
Thib <thibaut.g@protonmail.com>
2019-05-26 16:43:53 +0200

    Improve navigation

--- src/components/navbar/NavbarComponent.vue
+++ src/components/navbar/NavbarComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand href="#">Electathon 2019</b-navbar-brand>
+    <b-navbar-brand to="/">Electathon 2019</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
