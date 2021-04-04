c496e4d695c94e2794209a9f903b076a0e8ec4cc
Thibaut Goelff <thibaut@example.com>
2021-04-05 01:36:49 +0200

    Add bootstrap-vue inside brackets

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@/assets/style/app.css';
 
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
+import { BootstrapVue } from 'bootstrap-vue';
 
 import App from './App.vue';
 import router from './router';
